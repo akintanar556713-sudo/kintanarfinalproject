b5a16954071b54ea7eee493b2e9f343b0aacf5a0
astafurillah zutenburg <a.kintanar.556713@umindanao.edu.ph>
2026-05-30 14:09:37 +0800

    Update header.php

--- view/frame/header.php
+++ view/frame/header.php
@@ -18,7 +18,7 @@ $self = $_SERVER['SCRIPT_NAME'];
     <title>Asinstorage</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
-    <link rel="stylesheet" href="<?= BASE_URL ?>style.css">
+    <link rel="stylesheet" href="/style.css">
 </head>
 <body>
 
